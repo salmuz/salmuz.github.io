404bc1a6758cd1a1307890edd218a2827bd09f1e
Yonatan C. Carranza Alarcon <salmuz@gmail.com>
2020-10-26 10:49:46 +0100

    Cross validation upgrade

--- teaching/sy19/cross_validation_example.R
+++ teaching/sy19/cross_validation_example.R
@@ -20,6 +20,9 @@ data <- read.table('data/TPN1_a20_reg_app.txt')
 # mix up observations randomly
 data <- data[sample(nrow(data.fit)), ]
 
+############################################################
+# Nested and Non-nested Cross validation
+############################################################
 # creation de groupes pour la validation croisée 
 n <-  nrow(data)
 group.outer <- rep((1:10), (n/10)+1)[1:n]
@@ -38,11 +41,11 @@ for(i in 1:10){
   }
 }
 
-
-############################################################
-# Nested and Non-nested Cross validation
-############################################################
+################################################################
+## Regression
+################################################################
 library(FNN)
+# knn - linearregression
 Kmax <- 100
 K.sequences <- seq(1, Kmax, by = 2)
 err.knn.mse <-  rep(0, 10)
@@ -65,7 +68,9 @@ for(i in 1:10){
                            y=data.train.y, k=k)
       # on peut calculer la moyenne knn.mse.kmin 
       # (par chaque k, mais ils sont proportionnelle)
-      knn.mse.kmin[k] <- knn.mse.kmin[k] + mean((model.reg$pred-data.test.y)^2)
+      #1 - sum(diag(table(data.test.y, model.reg.pred)))/nrow(data.test.y)
+      knn.mse.kmin[k] <- knn.mse.kmin[k] + 
+            mean((model.reg$pred-data.test.y)^2)
     }
   } 
   idx.kmin <- which(min(knn.mse.kmin) == knn.mse.kmin)
@@ -84,6 +89,7 @@ for(i in 1:10){
 plot(knn.mse.kmin/10, type='l', ylab='Cv(k) Error', xlab="k voisins")
 boxplot(err.knn.mse)
 
+library(glmnet)
 # regression regularized 
 n.alpha <- 25
 err.reg.mse <-  rep(0, 10)
@@ -100,8 +106,11 @@ for(i in 1:10){
     alpha <- alphas[k]
     X.cv.train <- as.matrix(data.inner[, -101])
     y.cv.train <- data.inner[, 101]
-    cv.model <- cv.glmnet(X.cv.train, y.cv.train, alpha=alpha, foldid = group.inner)
-    err.lasso.mse[k] <- cv.model$cvm[which(cv.model$lambda == cv.model$lambda.min)]
+    cv.model <- cv.glmnet(X.cv.train, y.cv.train, alpha=alpha, 
+                          family = "multinomial",
+                          foldid = group.inner)
+    err.lasso.mse[k] <- cv.model$cvm[which(cv.model$lambda == 
+                                             cv.model$lambda.min)]
     best.lambda.alpha[k] <- cv.model$lambda.min
   }
   idx.best <- which(err.lasso.mse == min(err.lasso.mse))
@@ -112,7 +121,11 @@ for(i in 1:10){
   y.train <- data.inner[, 101]
   X.test <- as.matrix(data.validation[, -101])
   y.test <- data.validation[, 101]
-  model.fit <- glmnet(X.train, y.train, lambda = best.lambda, alpha = best.alpha)
+  model.fit <- glmnet(X.train, y.train, lambda = best.lambda, 
+                      family = "multinomial",
+                      alpha = best.alpha)
+  # classification
+  # table(y.test)
   err.reg.mse[i] <- mean((y.test - predict(model.fit, newx=X.test))^2)
 }
 boxplot(err.reg.mse)
@@ -127,3 +140,129 @@ plot.cv.error(errors, x.title = "Modèles")
 par(mfrow=c(1,2))
 plot.cv.error(as.matrix(errors[, 2]))
 boxplot(err.reg.mse)
+
+################################################################
+# forward/backward selection variable
+err.reg.mse <-  rep(0, 10)
+nb.variables.max <- 100
+for(i in 1:10){
+  index.outer.cv <- idx.test.outer[[i]]
+  data.inner <- data[-index.outer.cv,] # training 
+  data.validation <- data[index.outer.cv,] # validation
+  # re-sampling (inner cross validation) 
+  data.inner <- data.inner[rs.data.inner[[i]], ]
+  library(leaps) # library to selection of variables 
+  cv.errors <- matrix(NA, k, nb.variables.max)
+  cv.errors.train <- matrix(NA, k, nb.variables.max)
+  for(j in 1:10){ # cross validation interne
+    index.inner.cv <- idx.test.inner[[j]]
+    data.train <- data.inner[-index.inner.cv, ]
+    data.test <- data.inner[index.inner.cv, ]
+    best.models <- regsubsets(y~., data=data.train, 
+                              nvmax=nb.variables.max, 
+                              method="forward") # backward
+    for(v in 1:nb.variables.max){
+      pred <- predict(best.models, data.test, id=v)
+      pred.train <- predict(best.models, data.train, id=v)
+      cv.errors[j,v] <- mean((pred-data.test$y)^2)
+    }
+  }
+  # critère (one standard rule, moyenne minimal)
+  mse.min.regressors <- which.min(colMeans(cv.errors))
+  best.model <- regsubsets(y~., data=data.inner, nvmax = 100, 
+                      method = "forward")
+  #coef.best.model <- coef(best.model, mse.min.regressors)
+  pred <- predict(best.model, data.validation, id=mse.min.regressors)
+  err.reg.mse[i] <- mean((pred - data.validation$y)^2)
+}
+
+################################################################
+## Classification
+################################################################
+
+# reading data
+data <- read.table('data/TPN1_a20_clas_app.txt')
+# mix up observations randomly
+data <- data[sample(nrow(data)), ]
+data$y <- as.factor(data$y)
+
+# creation de groupes pour la validation croisée 
+n <-  nrow(data)
+group.outer <- rep((1:10), (n/10)+1)[1:n]
+idx.test.outer <- list()
+idx.test.inner <- list()
+rs.data.inner <- list()
+for(i in 1:10){
+  index.cv <- which(group.outer==i)
+  idx.test.outer[[i]] <- index.cv
+  n.inner <- n - length(index.cv)
+  rs.data.inner[[i]] <- sample(n.inner)
+  group.inner <- rep((1:10), (n.inner/10)+1)[1:n.inner]
+  for(j in 1:10){
+    index.cv <- which(group.inner==j)
+    idx.test.inner[[j]] <- index.cv
+  }
+}
+
+err <-  matrix(0, 10, 2)
+# regression logistic normal
+library(glmnet)
+for(i in 1:10){
+  index.outer.cv <- idx.test.outer[[i]]
+  data.inner <- data[-index.outer.cv,]
+  data.validation <- data[index.outer.cv,]
+  # re-sampling (inner cross validation)
+  data.inner <- data.inner[rs.data.inner[[i]], ]
+  
+  # inner cross-valition
+  X <- as.matrix(data.inner[, -51])
+  y <- as.factor(data.inner[, 51])
+  model.log <- glmnet(X, y, family = "multinomial", alpha = 1, lambda = 0)
+  
+  # validation
+  pred <- predict(model.log, newx=as.matrix(data.validation[, -51]), 
+                  type="response")
+  confusion <- table(data.validation[, 51], max.col(pred[,,]))
+  err[i, 1] <- 1 - sum(diag(confusion))/nrow(data.validation)
+}
+
+
+# knn classification
+library(FNN)
+Kmax <- 15
+K.sequences <- seq(1, Kmax, by = 2)
+for(i in 1:10){
+  index.outer.cv <- idx.test.outer[[i]]
+  data.inner <- data[-index.outer.cv,]
+  data.validation <- data[index.outer.cv,]
+  # re-sampling (inner cross validation)
+  data.inner <- data.inner[rs.data.inner[[i]], ]
+  # cross-validation interne 
+  knn.mse.kmin <- rep(0, length(K.sequences))
+  for(k in 1:length(K.sequences)){
+    for(j in 1:10){
+      index.inner.cv <- idx.test.inner[[j]]
+      data.train.x <- data.inner[-index.inner.cv, -51]
+      data.train.y <- data.inner[-index.inner.cv, 51]
+      data.test.x <- data.inner[index.inner.cv, -51]
+      data.test.y <- data.inner[index.inner.cv, 51]
+      model.reg <- knn(train=data.train.x, test=data.test.x, 
+                       cl=data.train.y, k=k)
+      errc <- 1 - sum(diag(table(data.test.y, model.reg)))/nrow(data.inner)
+      knn.mse.kmin[k] <- knn.mse.kmin[k] + errc
+    }
+  } 
+  idx.kmin <- which(min(knn.mse.kmin) == knn.mse.kmin)
+  best.kmin <- K.sequences[idx.kmin]
+  
+  # validation our model with best model 
+  data.train.x <- data.inner[, -51]
+  data.train.y <- data.inner[, 51]
+  data.test.x <- data.validation[, -51]
+  data.test.y <- data.validation[, 51]
+  model.reg <- knn(train=data.train.x, test=data.test.x, 
+                   cl=data.train.y, k=best.kmin)
+  n <- nrow(data.validation)
+  err[i, 2] <- 1 - sum(diag(table(data.test.y, model.reg)))/n
+}
+
